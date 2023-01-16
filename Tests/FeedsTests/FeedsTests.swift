54f2a7f156349ed1cd60a2ad028f3a28750cc4fe
Stepan Valchyshyn <DX0YR@intra.coop>
2023-01-17 00:56:33 +0200

    Initial Commit

---
+++ Tests/FeedsTests/FeedsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Feeds
+
+final class FeedsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Feeds().text, "Hello, World!")
+    }
+}
